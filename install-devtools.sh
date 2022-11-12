3a1b8eedd4f29b562daa1b8c41682e6bf8c41200
nicomiguelino <nicomiguelino2014@gmail.com>
2022-11-11 17:55:09 -0800

    Add git as a dependency to the installation script.

--- install-devtools.sh
+++ install-devtools.sh
@@ -5,6 +5,7 @@ ANSIBLE_VERSION="ansible-core==2.12"
 if ! [ "$(which ansible)"  ]; then
     sudo apt update -y && \
     sudo apt-get install -y --no-install-recommends \
+        git \
         python3 \
         python3-dev \
         python3-pip
