2f80fdffe99cdddbc7616d6e51b727268c7018f5
nicomiguelino <nicomiguelino2014@gmail.com>
2023-01-10 18:24:17 -0800

    Refactor download and extraction of `wifi-connect` archive.
    - Make use of build-time arguments.
    - The archive file that will be downloaded and extracted will depend on
      the build argument that will be set for the architecture.

---
+++ jq_poc/bin/install_wifi_connect.sh
@@ -0,0 +1,12 @@
+if [[ "$TARGET_PLATFORM" = "linux/arm/v6" ]]; then
+    architecture='rpi'
+elif [[ "$TARGET_PLATFORM" = "linux/arm/v7" ]]; then
+    architecture='armv7hf'
+fi
+
+jq_filter=".assets[] | select (.name|test(\"linux-$architecture\")) | .browser_download_url"
+archive_url=$(curl -sL $WC_DL_URL | jq -r "$jq_filter")
+archive_file=$(basename $archive_url)
+
+wget $archive_url && \
+tar -xvz -C /usr/src/app -f $archive_file
