99ae4d83ab2359783bfd62faa299f9eba35df8cc
nicomiguelino <nicomiguelino2014@gmail.com>
2023-01-16 14:54:19 -0800

    Write a mocked version of the test case that we've created earlier.

--- testing-poc/tests/test_delete.py
+++ testing-poc/tests/test_delete.py
@@ -1,6 +1,7 @@
 import os.path
 import tempfile
 import unittest
+from unittest import mock
 
 from src.delete import rm
 
@@ -12,7 +13,7 @@ class RmTestCase(unittest.TestCase):
         with open(self.tmpfilepath, 'wb') as f:
             f.write(b'Delete me!')
 
-    def test_rm(self):
+    def test_rm_1(self):
         # Remove the file.
         rm(self.tmpfilepath)
 
@@ -21,3 +22,9 @@ class RmTestCase(unittest.TestCase):
             os.path.isfile(self.tmpfilepath),
             'Failed to remove the file.',
         )
+
+    @mock.patch('src.delete.os')
+    def test_rm_2(self, mock_os):
+        rm('any path')
+        # Test that rm called os.remove with the right parameters.
+        mock_os.remove.assert_called_with('any path')
