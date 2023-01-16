4d49356655c78bc49cfaac75c3a88c1aa06fb04c
nicomiguelino <nicomiguelino2014@gmail.com>
2023-01-16 15:25:18 -0800

    Create a class-based file removal service.

--- testing-poc/src/delete.py
+++ testing-poc/src/delete.py
@@ -5,3 +5,11 @@ import os.path
 def rm(filename):
     if os.path.isfile(filename):
         os.remove(filename)
+
+
+class RemovalService:
+    """A service for removing objects from the filesystem."""
+
+    def rm(filename):
+        if os.path.isfile(filename):
+            os.remove(filename)
